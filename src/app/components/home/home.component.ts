578e14a7b285e5d79ae3c20233be1834e1baa17b
lkribeirof <116752823+lkribeirof@users.noreply.github.com>
2024-07-14 18:24:29 -0300

    adicionado varios components

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { NgOptimizedImage } from '@angular/common';
+import { BtnPrimaryComponent } from '../btn-primary/btn-primary.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, NgOptimizedImage, BtnPrimaryComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
